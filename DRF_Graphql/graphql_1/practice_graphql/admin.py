8db6ad330036220fc1668770396dd1cb4a473458
oereo <dlstpgns0406@gmail.com>
2020-10-26 12:46:47 +0900

    [DELETE] cached

--- DRF_Graphql/graphql_1/practice_graphql/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from practice_graphql.models import Actor, Movie
-
-admin.site.register(Actor)
-admin.site.register(Movie)
--- DRF_Graphql/graphql_1/practice_graphql/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PracticeGraphqlConfig(AppConfig):
-    name = 'practice_graphql'
-    verbose_name = 'A Much Better Name'
