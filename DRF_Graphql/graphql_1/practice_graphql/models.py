93a15f310aad232fcb069cd0e148213618562653
oereo <dlstpgns0406@gmail.com>
2020-10-27 18:32:41 +0900

    [ADD] comments

--- DRF_Graphql/graphql_1/practice_graphql/models.py
+++ DRF_Graphql/graphql_1/practice_graphql/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Actor(models.Model):
     name = models.CharField(max_length=100)
+    age = models.IntegerField()
 
     def __str__(self):
         return self.name
